2f0b4d603876a745f8ae16b9ec6d817edfabf35c
begüm muşdal <begum_musdal@hotmail.com>
2021-01-21 20:56:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] _keys;
+        V[] _values;
+        public MyDictionary()
+        {
+            _keys = new K[0];
+            _values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = _keys;
+            V[] tempValues = _values;
+            _keys = new K[_keys.Length + 1];
+            _values = new V[_values.Length + 1];
+            for (int i = 0; i < tempKeys.Length && i<tempValues.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _values.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> idName = new MyDictionary<int, string>();
+            idName.Add(1, "Nisa");
+            idName.Add(2, "Begüm");
+            idName.Add(3, "Gizem");
+            idName.Add(4, "İrem");
+            
+            Console.WriteLine(idName.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
